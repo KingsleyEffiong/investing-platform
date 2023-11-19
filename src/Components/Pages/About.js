56c23522c8ff053b04d59bc6647800fd011b5e03
Divine <divinetidy@gmail.com>
2023-11-19 03:11:09 +0100

    added about

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,13 +1,85 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 function About() {
   return (
     <>
-    <div className='containers'>
-    <h1>Our Mission</h1>
-    </div>
+      <div className="containers">
+        <div className="flex_container">
+          <div className="image"></div>
+          <h3>John Doe</h3>
+          <h5>CEO & Founder</h5>
+          <p>
+            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse
+            sodales pellentesque elementum.
+          </p>
+          <a href="">Message</a>
+        </div>
+        <div className="flex_container">
+          <div className="image"></div>
+          <h3>John Doe</h3>
+          <h5>CEO & Founder</h5>
+          <p>
+            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse
+            sodales pellentesque elementum.
+          </p>
+          <a href="">Message</a>
+        </div>
+        <div className="flex_container">
+          <div className="image"></div>
+          <h3>John Doe</h3>
+          <h5>CEO & Founder</h5>
+          <p>
+            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse
+            sodales pellentesque elementum.
+          </p>
+          <a href="">Message</a>
+        </div>
+        <div className="flex_container">
+          <div className="image"></div>
+          <h3>John Doe</h3>
+          <h5>CEO & Founder</h5>
+          <p>
+            Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse
+            sodales pellentesque elementum.
+          </p>
+          <a href="">Message</a>
+        </div>
+      </div>
+      <div className="happy_client_container">
+        <div className="client">
+          <h2>
+            14<span className="plus">+</span>
+          </h2>
+          <br></br>
+          <span className="partners">partners</span>
+        </div>
+        <div className="client">
+          <h2>
+            2,000<span className="plus">+</span>
+          </h2>
+          <br></br>
+          <span className="partners">Investors</span>
+        </div>
+        <div className="client">
+          <h2>
+            100<span className="plus">%</span>
+          </h2>
+          <br></br>
+          <span className="partners">Happy client</span>
+        </div>
+        <div className="client">
+          <h2>
+            1,700<span className="plus">+</span>
+          </h2>
+          <br></br>
+          <span className="partners">Meetings</span>
+        </div>
+      </div>
+      <footer>
+        <a href="">© Powered by InvestAmerica.</a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
